383ea95660a9bbec5ff5c50a8bbf8dce464a3808
Luis Alexandre Rosa da Silva <luisalexandre6668@gmail.com>
2022-04-11 21:02:08 -0300

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -18,7 +18,7 @@ class PongGame:
         run = True
         clock = pygame.time.Clock()
         while run:
-            clock.tick(120)
+            clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
